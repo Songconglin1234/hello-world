aad97be96eabd22dc9c66b26059b265fb3217db8
songcl <songconglin1234@gmail.com>
2020-04-27 09:12:52 +0800

    create table python

---
+++ sqldb_op.py
@@ -0,0 +1,69 @@
+import MySQLdb
+import pymysql
+
+DATABASE_HOUSEINFO = {
+    'host':'127.0.0.1',
+    'database':'houseinfo',
+    'user':'root',
+    'password':'test',
+    'charset':'utf8mb4'
+    }
+DATABASE_FLIGHTS = {
+    'host':'127.0.0.1',
+    'database':'flights',
+    'user':'root',
+    'password':'test',
+    'charset':'utf8mb4'
+    }
+
+conn = MySQLdb.connect(host = 'localhost',user = 'root',password = 'test',db = 'flights')
+   
+mycursor  = conn.cursor()
+
+'''
+    CREATE TABLE `flights` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`origin` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`destination` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	duration INT(11) NOT NULL,
+	PRIMARY KEY (id)
+    );
+'''
+sql_createTb = """CREATE TABLE `flights` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`origin` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`destination` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	duration INT(11) NOT NULL,
+	PRIMARY KEY (id)
+    );"""
+
+mycursor.execute(sql_createTb)
+conn.commit()
+ 
+'''
+sql_createTb
+CREATE TABLE `passagers` (
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`passager_name` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+flights_id INT(11) NOT NULL REFERENCES flights(id),
+PRIMARY KEY (id)
+)
+'''
+
+sql_createTb = """CREATE TABLE `passagers` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+   `passager_name` VARCHAR(80) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+   flights_id INT(11) NOT NULL REFERENCES flights(id), 
+   PRIMARY KEY (id))"""
+
+mycursor.execute(sql_createTb)
+conn.commit()
+   
+
+
+
+'''
+results = cursor.fetchall()
+for row in results:
+    print(row)
+'''
